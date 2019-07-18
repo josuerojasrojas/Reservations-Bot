dd44ccf4679a4c408eecd98fb760dcc5796fc926
josue rojas <josuerojas.rojas@gmail.com>
2019-07-18 11:13:17 -0400

    added catch for fetc

--- client/src/App.js
+++ client/src/App.js
@@ -19,7 +19,8 @@ class App extends Component {
       .then(res => res.json())
       .then(reservations =>
         this.setState({ reservations: reservations, numReservations: reservations.length }
-        ));
+        ))
+      .catch((e) => console.log('error', e));
     let $this = this;
     socket.on('reservations', function(data){
       let reservations = [ ...$this.state.reservations ];
